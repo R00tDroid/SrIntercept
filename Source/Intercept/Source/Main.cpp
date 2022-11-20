555fc1c140d277ec4f205cbdbc229536f682cc5e
R00tDroid <info@rootdroid.nl>
2022-11-20 15:13:52 +0100

    Setup project with Detours dependency

---
+++ Source/Intercept/Source/Main.cpp
@@ -0,0 +1,20 @@
+#include <windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE, DWORD Event, LPVOID) 
+{
+    switch(Event)
+    { 
+        case DLL_PROCESS_ATTACH:
+            break;
+
+        case DLL_THREAD_ATTACH:
+            break;
+
+        case DLL_THREAD_DETACH:
+            break;
+
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+    return TRUE;
+}
