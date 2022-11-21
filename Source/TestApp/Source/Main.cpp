36fc3adad6d17f0b1d10a30e4394120ae4c7436a
R00tDroid <info@rootdroid.nl>
2022-11-21 21:54:05 +0100

    Create window

--- Source/TestApp/Source/Main.cpp
+++ Source/TestApp/Source/Main.cpp
@@ -1,7 +1,34 @@
 #include <d3d11.h>
 
+HWND window = nullptr;
 ID3D11Device* d3dDevice = nullptr;
 ID3D11DeviceContext* d3dContext = nullptr;
+
+bool InitWindow()
+{
+    WNDCLASSEXA winClass = {};
+    winClass.cbSize = sizeof(WNDCLASSEXA);
+    winClass.style = CS_HREDRAW | CS_VREDRAW;
+    winClass.lpfnWndProc = &DefWindowProc;
+    winClass.hInstance = GetModuleHandleA(nullptr);
+    winClass.lpszClassName = "WindowClass";
+
+    RegisterClassExA(&winClass);
+
+    window = CreateWindowExA(WS_EX_OVERLAPPEDWINDOW, winClass.lpszClassName, "", WS_OVERLAPPEDWINDOW | WS_VISIBLE,CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, 0, 0, GetModuleHandleA(nullptr), 0);
+
+    return true;
+}
+
+void UpdateWindow()
+{
+    MSG message;
+    while (PeekMessageA(&message, window, 0, 0, true))
+    {
+        TranslateMessage(&message);
+        DispatchMessageA(&message);
+    }
+}
 
 bool InitD3D()
 {
@@ -18,7 +45,13 @@ bool InitD3D()
 
 int main()
 {
+    if (!InitWindow()) return -1;
     if (!InitD3D()) return -1;
+
+    while (true)
+    {
+        UpdateWindow();
+    }
 
     return 0;
 }
