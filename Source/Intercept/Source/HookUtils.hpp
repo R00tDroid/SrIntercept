04db600cd3fbba97d65cdfd8a859690b60cc7c64
RootDroid <42697021+R00tDroid@users.noreply.github.com>
2022-11-23 22:31:58 +0100

    feature/hook-testapp (#4)
    Co-authored-by: R00tDroid <info@rootdroid.nl>

--- Source/Intercept/Source/HookUtils.hpp
+++ Source/Intercept/Source/HookUtils.hpp
@@ -2,20 +2,15 @@
 #include <detours.h>
 #include <windows.h>
 
-template<typename T>
-void AttachFunction(const char* moduleName, const char* functionSignature, T& originalFunction, void* overrideFunction)
+typedef void* FunctionPointer;
+
+FunctionPointer FindFunction(const char* moduleName, const char* functionSignature)
 {
-    AttachFunction<T>(DetourFindFunction(moduleName, functionSignature), originalFunction, overrideFunction);
+    return DetourFindFunction(moduleName, functionSignature);
 }
 
 template<typename T>
-void AttachFunction(DWORD functionAddress, T& originalFunction, void* overrideFunction)
-{
-    AttachFunction<T>(reinterpret_cast<void*>(functionAddress), originalFunction, overrideFunction);
-}
-
-template<typename T>
-void AttachFunction(void* target, T& originalFunction, void* overrideFunction)
+void AttachFunction(FunctionPointer target, T& originalFunction, void* overrideFunction)
 {
     originalFunction = reinterpret_cast<T>(target);
     DetourAttach(&static_cast<void*>(originalFunction), overrideFunction);
--- Source/Intercept/Source/Main.cpp
+++ Source/Intercept/Source/Main.cpp
@@ -5,7 +5,7 @@
 typedef void(__thiscall* DX11WeaverBase_Weave_t)(void* __this, unsigned int width, unsigned int height);
 DX11WeaverBase_Weave_t DX11WeaverBase_Weave;
 
-void __fastcall Override_DX11WeaverBase_Weave(void* __this, unsigned int width, unsigned int height)
+__declspec(dllexport) void __fastcall Override_DX11WeaverBase_Weave(void* __this, unsigned int width, unsigned int height)
 {
     DX11WeaverBase_Weave(__this, width, height);
 }
@@ -19,7 +19,15 @@ BOOL WINAPI DllMain(HINSTANCE, DWORD Event, LPVOID)
             DetourRestoreAfterWith();
             DetourTransactionBegin();
             DetourUpdateThread(GetCurrentThread());
-            AttachFunction<DX11WeaverBase_Weave_t>("SimulatedRealityDirectX.dll", "SR::DX11WeaverBase::Weave", DX11WeaverBase_Weave, Override_DX11WeaverBase_Weave);
+
+            FunctionPointer weave = FindFunction("SimulatedRealityDirectX.dll", "?weave@DX11WeaverBase@SR@@QEAAXII@Z");
+            if (weave == nullptr)
+            {
+                MessageBoxA(nullptr, "Failed to hook SR::DX11WeaverBase::Weave", "", MB_OK + MB_ICONERROR);
+                exit(-1);
+            }
+
+            AttachFunction<DX11WeaverBase_Weave_t>(weave, DX11WeaverBase_Weave, Override_DX11WeaverBase_Weave);
             DetourTransactionCommit();
             break;
         }
