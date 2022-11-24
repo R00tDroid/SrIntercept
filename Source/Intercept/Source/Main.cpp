cd6d1605367a6f32eb3f1c7d1bd7a30ca650a816
R00tDroid <info@rootdroid.nl>
2022-11-24 18:37:56 +0100

    Get access to the weaver framebuffer

--- Source/Intercept/Source/Main.cpp
+++ Source/Intercept/Source/Main.cpp
@@ -1,13 +1,31 @@
+#include <d3d11.h>
 #include <windows.h>
 #include <detours.h>
 #include "HookUtils.hpp"
 
-typedef void(__thiscall* DX11WeaverBase_Weave_t)(void* __this, unsigned int width, unsigned int height);
-DX11WeaverBase_Weave_t DX11WeaverBase_Weave;
+typedef void(__thiscall* DX11WeaverBase_weave_t)(void* object, unsigned int width, unsigned int height);
+DX11WeaverBase_weave_t DX11WeaverBase_weave;
 
-__declspec(dllexport) void __fastcall Override_DX11WeaverBase_Weave(void* __this, unsigned int width, unsigned int height)
+typedef ID3D11RenderTargetView*(__thiscall* DX11WeaverBase_getFrameBuffer_t)(void* object);
+DX11WeaverBase_getFrameBuffer_t DX11WeaverBase_getFrameBuffer;
+
+__declspec(dllexport) void __fastcall Override_DX11WeaverBase_weave(void* object, unsigned int width, unsigned int height)
 {
-    DX11WeaverBase_Weave(__this, width, height);
+    ID3D11RenderTargetView* frameBuffer = DX11WeaverBase_getFrameBuffer(object);
+    if (frameBuffer != nullptr)
+    {
+        D3D11_RENDER_TARGET_VIEW_DESC Desc;
+        frameBuffer->GetDesc(&Desc);
+
+        ID3D11Texture2D* Texture = nullptr;
+        frameBuffer->GetResource((ID3D11Resource**)&Texture);
+
+        D3D11_TEXTURE2D_DESC TexDesc;
+        Texture->GetDesc(&TexDesc);
+
+        Texture->Release();
+    }
+    DX11WeaverBase_weave(object, width, height);
 }
 
 BOOL WINAPI DllMain(HINSTANCE, DWORD Event, LPVOID) 
@@ -23,11 +41,19 @@ BOOL WINAPI DllMain(HINSTANCE, DWORD Event, LPVOID)
             FunctionPointer weave = FindFunction("SimulatedRealityDirectX.dll", "?weave@DX11WeaverBase@SR@@QEAAXII@Z");
             if (weave == nullptr)
             {
-                MessageBoxA(nullptr, "Failed to hook SR::DX11WeaverBase::Weave", "", MB_OK + MB_ICONERROR);
+                MessageBoxA(nullptr, "Failed to hook SR::DX11WeaverBase::weave", "", MB_OK + MB_ICONERROR);
                 exit(-1);
             }
 
-            AttachFunction<DX11WeaverBase_Weave_t>(weave, DX11WeaverBase_Weave, Override_DX11WeaverBase_Weave);
+            FunctionPointer getFrameBuffer = FindFunction("SimulatedRealityDirectX.dll", "?getFrameBuffer@DX11WeaverBase@SR@@QEAAPEAUID3D11RenderTargetView@@XZ");
+            if (getFrameBuffer == nullptr)
+            {
+                MessageBoxA(nullptr, "Failed to hook SR::DX11WeaverBase::getFrameBuffer", "", MB_OK + MB_ICONERROR);
+                exit(-1);
+            }
+            DX11WeaverBase_getFrameBuffer = static_cast<DX11WeaverBase_getFrameBuffer_t>(getFrameBuffer);
+
+            AttachFunction<DX11WeaverBase_weave_t>(weave, DX11WeaverBase_weave, Override_DX11WeaverBase_weave);
             DetourTransactionCommit();
             break;
         }
