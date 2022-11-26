2d97fed656fc38d9259db96aa5a9377672baeb4c
R00tDroid <info@rootdroid.nl>
2022-11-26 17:17:28 +0100

    Reference dll with an absolute path

--- Source/Injector/Source/Main.cpp
+++ Source/Injector/Source/Main.cpp
@@ -1,8 +1,15 @@
 #include <windows.h>
 #include "detours.h"
+#include <filesystem>
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, PSTR Command, int)
 {
+    char exePath[MAX_PATH];
+    GetModuleFileNameA(GetModuleHandleA(nullptr), exePath, MAX_PATH);
+    std::filesystem::path binaryDirectory = std::filesystem::path(exePath).parent_path();
+
+    std::filesystem::path dllPath = binaryDirectory / "SrIntercept.dll";
+
     STARTUPINFOA startupInfo;
     PROCESS_INFORMATION processInfo;
 
@@ -12,7 +19,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, PSTR Command, int)
     startupInfo.wShowWindow = SW_SHOW;
     ZeroMemory(&processInfo, sizeof(processInfo));
 
-    if (DetourCreateProcessWithDllExA(Command, nullptr, nullptr, nullptr, false, CREATE_DEFAULT_ERROR_MODE | CREATE_SUSPENDED, nullptr, nullptr, &startupInfo, &processInfo, "Intercept.dll", nullptr))
+    if (DetourCreateProcessWithDllExA(Command, nullptr, nullptr, nullptr, false, CREATE_DEFAULT_ERROR_MODE | CREATE_SUSPENDED, nullptr, nullptr, &startupInfo, &processInfo, dllPath.string().c_str(), nullptr))
     {
         //TODO Log success
     }
