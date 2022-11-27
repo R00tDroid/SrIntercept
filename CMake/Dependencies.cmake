4f8a662719472bab85216e795581c949bbc7e946
R00tDroid <info@rootdroid.nl>
2022-11-27 16:59:03 +0100

    Add clsocket dependency

--- CMake/Dependencies.cmake
+++ CMake/Dependencies.cmake
@@ -12,3 +12,27 @@ add_library(Detours STATIC
     ${DetoursSource}
 )
 set_target_properties(Detours PROPERTIES FOLDER "Dependencies")
+
+CPMAddPackage(
+    NAME clsocket
+    GITHUB_REPOSITORY DFHack/clsocket
+    GIT_TAG origin/master
+    DOWNLOAD_ONLY TRUE
+)
+
+set(ClsocketSource
+    ${clsocket_SOURCE_DIR}/src/ActiveSocket.h
+    ${clsocket_SOURCE_DIR}/src/ActiveSocket.cpp
+    ${clsocket_SOURCE_DIR}/src/Host.h
+    ${clsocket_SOURCE_DIR}/src/PassiveSocket.h
+    ${clsocket_SOURCE_DIR}/src/PassiveSocket.cpp
+    ${clsocket_SOURCE_DIR}/src/SimpleSocket.h
+    ${clsocket_SOURCE_DIR}/src/SimpleSocket.cpp
+    ${clsocket_SOURCE_DIR}/src/statTimer.h
+)
+
+add_library(clsocket STATIC
+    ${ClsocketSource}
+)
+target_compile_options(clsocket PUBLIC /WX- /W1)
+set_target_properties(clsocket PROPERTIES FOLDER "Dependencies")
